069fb645ff9adfe4fe07d69d0919c87bf147071e
Chris Oliver <excid3@gmail.com>
2009-12-29 16:19:19 -0600

    Added template for main view

--- pastebin/views.py
+++ pastebin/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.template import Context, loader
+from django.http import HttpResponse
+
+def main(request):
+    t = loader.get_template('pastebin/templates/index.html')
+    c = Context(locals())
+    return HttpResponse(t.render(c))
