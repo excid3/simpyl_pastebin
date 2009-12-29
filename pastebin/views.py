b13f7480fa8b8206210c94c08bb323d5c79359fc
Chris Oliver <excid3@gmail.com>
2009-12-29 16:52:34 -0600

    Added pastebin main view

--- pastebin/views.py
+++ pastebin/views.py
@@ -2,6 +2,11 @@ from django.template import Context, loader
 from django.http import HttpResponse
 
 def main(request):
+    previous = request.POST.get('paste', '')
+    
     t = loader.get_template('pastebin/templates/index.html')
-    c = Context(locals())
+    c = Context({
+        'previous': previous
+    })
+    
     return HttpResponse(t.render(c))
